b0d52f5a2bee31cdfed47aa50a7da328208159b6
Orestis Meikopoulos <diktyas1988@gmail.com>
2024-04-08 23:35:46 +0300

    Fix exposedServices and try again for provisioning app insights resource

--- GlobalAzure.NetAspire.AppHost/Program.cs
+++ GlobalAzure.NetAspire.AppHost/Program.cs
@@ -3,9 +3,10 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Automatically provision an Application Insights resource
-var insights = builder.ExecutionContext.IsPublishMode
-    ? builder.AddAzureApplicationInsights("aspiredemoapplicationinsights")
-    : builder.AddConnectionString("aspiredemoapplicationinsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
+//var insights = builder.ExecutionContext.IsPublishMode
+//    ? builder.AddAzureApplicationInsights("aspiredemoapplicationinsights")
+//    : builder.AddConnectionString("aspiredemoapplicationinsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
+var insights = builder.AddAzureApplicationInsights("aspiredemoapplicationinsights");
 
 // Provisions an Azure SQL Database when published
 var customerDb = builder
