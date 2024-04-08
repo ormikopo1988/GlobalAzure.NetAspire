264620ba0c353579bbcfbdd1ba591f000c03fef7
Orestis Meikopoulos <diktyas1988@gmail.com>
2024-04-08 23:55:39 +0300

    Let's try again

--- GlobalAzure.NetAspire.AppHost/Program.cs
+++ GlobalAzure.NetAspire.AppHost/Program.cs
@@ -3,10 +3,9 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Automatically provision an Application Insights resource
-//var insights = builder.ExecutionContext.IsPublishMode
-//    ? builder.AddAzureApplicationInsights("aspiredemoapplicationinsights")
-//    : builder.AddConnectionString("aspiredemoapplicationinsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
-var insights = builder.AddAzureApplicationInsights("aspiredemoapplicationinsights");
+var insights = builder.ExecutionContext.IsPublishMode
+    ? builder.AddAzureApplicationInsights("aspiredemoapplicationinsights")
+    : builder.AddConnectionString("aspiredemoapplicationinsights", "APPLICATIONINSIGHTS_CONNECTION_STRING");
 
 // Provisions an Azure SQL Database when published
 var customerDb = builder
