0ede1bb65eedade60c7dccc6f4075d90907e0b24
Orestis Meikopoulos <diktyas1988@gmail.com>
2025-03-03 19:34:54 +0200

    Integrate OpenApi instead of Swagger

--- src/GlobalAzure.NetAspire.Api/Program.cs
+++ src/GlobalAzure.NetAspire.Api/Program.cs
@@ -23,17 +23,16 @@ builder.Services.AddHttpClient("GitHub", httpClient =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
--- src/GlobalAzure.NetAspire.Server/Program.cs
+++ src/GlobalAzure.NetAspire.Server/Program.cs
@@ -46,8 +46,9 @@ builder.Services.AddIdentityCore<ApplicationUser>()
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -61,8 +62,7 @@ app.ApplyMigrations();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
     app.SeedData();
 }
 
